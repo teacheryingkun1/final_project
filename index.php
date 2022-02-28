4605dd522e9bbb6f9240034cefa2ae7ffe2602eb
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-02-28 10:35:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
+    <title>อย่าลืมแก้ไตเติ้ล</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sarabun">
     <style>
     	body{
@@ -67,6 +67,13 @@
         </div>
         <div class="col-10" id="content"> <!-- คอนเท้นต์ -->
           <center><h1>Content</h1></center>
+	  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+		  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+		  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		  It has survived not only five centuries, but also the leap into electronic typesetting, 
+		  remaining essentially unchanged. 
+		  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+		  and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
       </div>
        <div class="row">
