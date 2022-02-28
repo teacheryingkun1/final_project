7e9100b790dbd9191e3ea29ef5c4a859cb2fa958
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-02-28 10:26:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		
+	</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sarabun">
+	<style type="text/css">
+		body{
+			font-family: 'Sarabun';
+		}
+	</style>
+</head>
+<body>
+	<h1>สวัสดีจ้าาาา ... </h1>
+</body>
+</html>
